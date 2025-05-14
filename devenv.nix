5c9dc284e89558d2a7e1dc28bd4874bcd8d5e80a
Cian Hughes <chughes000@gmail.com>
2025-05-14 12:15:41 +0100

    Added bun and js tooling for better automation

--- devenv.nix
+++ devenv.nix
@@ -7,13 +7,20 @@
 }: {
   packages = [
     pkgs.git
+    pkgs.bun
     pkgs.act
   ];
 
-  languages.python = {
-    enable = true;
-    uv = {
+  languages = {
+    python = {
       enable = true;
+      uv = {
+        enable = true;
+      };
+    };
+    javascript.bun = {
+      enable = true;
+      install.enable = true;
     };
   };
 }
---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+const config = {
+  singleQuote: false,
+  tabWidth: 4,
+  trailingComma: "all",
+  printWidth: 100,
+  plugins: ["prettier-plugin-jinja-template"],
+  overrides: [
+    {
+      files: ["*.html"],
+      options: {
+        parser: "jinja-template"
+      }
+    }
+  ]
+};
+
+export default config;
